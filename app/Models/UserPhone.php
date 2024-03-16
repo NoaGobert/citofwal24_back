916cc1108297555333392d89931b5587d426f56e
Sébastien Merveille <sebastien.merveille@ac.andenne.be>
2024-03-16 11:48:14 +0100

    Change names of Models

--- app/Models/UserPhone.php
+++ app/Models/UserPhone.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class food_schedule extends Model
+class UserPhone extends Model
 {
     use HasFactory;
 }

e608620531705c88c3c710b9ac3a4162e954934e
Noa Gobert <nonocheclub@gmail.com>
2024-03-16 19:39:10 +0100

    début de foodstatus controller

--- app/Models/FoodStatus.php
+++ app/Models/FoodStatus.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class FoodStatus extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'status',
+    ];
+
+
+    public function food()
+    {
+        return $this->hasMany(Food::class, 'uuid', 'foods_uuid');
+    }
 }

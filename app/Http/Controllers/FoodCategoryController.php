2f9843325f3570efc741c73e4ad62f1a632d2659
Sébastien Merveille <sebastien.merveille@ac.andenne.be>
2024-03-16 15:40:28 +0100

    Crud FootCategory - Food (todo : Groups for the crud of food)

---
+++ app/Http/Controllers/FoodCategoryController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FoodCategory;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class FoodCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $category = FoodCategory::where('is_active', '=', true)->get();
+
+
+        if (!$category) {
+            return response()->json([
+                'message' => 'Category not found'
+            ], 404);
+        }
+
+        return $category;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'name' => 'required',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation error',
+                'errors' => $e->errors()
+            ], 400);
+        }
+
+        $category = FoodCategory::create($validated);
+
+        return $category;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = FoodCategory::where('is_active', '=', true)->find($id);
+
+        if (!$category) {
+            return response()->json([
+                'message' => 'Category not found'
+            ], 404);
+        }
+
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        try {
+            $validated = $request->validate([
+                'name' => 'required',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation error',
+                'errors' => $e->errors()
+            ], 400);
+        }
+
+        $category = FoodCategory::where('is_active', '=', true)->find($id);
+
+        if (!$category) {
+            return response()->json([
+                'message' => 'Category not found'
+            ], 404);
+        }
+
+        $category->update($validated);
+
+        return $category;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = FoodCategory::find($id);
+
+        if (!$category) {
+            return response()->json([
+                'message' => 'Category not found'
+            ], 404);
+        }
+
+        $category->is_active = false;
+        $category->save();
+
+        return $category;
+    }
+}

8f5ac0457bc7837c37a4c4f3cc38f2b1fbd85eb6
Sébastien Merveille <sebastien.merveille@ac.andenne.be>
2024-03-16 18:54:44 +0100

    Crud user adresses

--- app/Http/Controllers/UserAdressesController.php
+++ app/Http/Controllers/UserAdressesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\UserAddress;
 use Illuminate\Http\Request;
 
 class UserAdressesController extends Controller
@@ -11,5 +12,19 @@ class UserAdressesController extends Controller
     {
         $user = User::where('is_active', 1)->find($id);
         return $user;
+    }
+
+    public function store(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->addresses()->create($request->all());
+        return $user->addresses;
+    }
+
+    public function destroy($id)
+    {
+        $adress = UserAddress::find($id);
+        $adress->is_active = 0;
+        $adress->save();
     }
 }
